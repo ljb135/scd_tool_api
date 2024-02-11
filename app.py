f5f3bc525f4872675f91a79e055b78054f4dd664
Chris Liang <christopherliang.cl@gmail.com>
2024-02-10 23:55:07 -0500

    Moved Flask routes to new file

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, Response, jsonify
-from database import db, User
-from sqlalchemy import select
+from flask import Flask
+from database import db
+from routes.user import user_routes
 
 
 app = Flask(__name__)
@@ -11,41 +11,4 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-
-@app.route("/user", methods=['POST'])
-def add_user():
-    content = request.json
-    user = User(**content)
-    db.session.add(user)
-    db.session.commit()
-    return Response("User has been created.", status=201)
-
-
-@app.route("/user", methods=['GET'])
-def get_all_users():
-    users = db.session.scalars(select(User))
-    return jsonify(list(users))
-
-
-@app.route("/user/<int:id>", methods=['GET'])
-def get_user(user_id):
-    user = db.get_or_404(User, user_id)
-    return jsonify(user)
-
-
-@app.route("/user/<int:id>", methods=['PUT'])
-def modify_user(user_id):
-    content = request.json
-    user = db.get_or_404(User, user_id)
-    for key in content:
-        setattr(user, key, content[key])
-    db.session.commit()
-    return Response("User has been updated.", status=200)
-
-
-@app.route("/user/<int:id>", methods=['DELETE'])
-def delete_user(user_id):
-    user = db.get_or_404(User, user_id)
-    db.session.delete(user)
-    db.session.commit()
-    return Response("User has been deleted.", status=200)
+app.register_blueprint(user_routes)

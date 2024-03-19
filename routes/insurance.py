8c90b1ecac2ae9f83b31cf6b868ecfe45e61073a
Chris Liang <christopherliang.cl@gmail.com>
2024-03-19 18:36:13 -0400

    Modified insurance endpoint to return only id and name

--- routes/insurance.py
+++ routes/insurance.py
@@ -7,4 +7,4 @@ insurance_routes = Blueprint('insurance', __name__, url_prefix='/insurance')
 @insurance_routes.route("/", methods=['GET'])
 def get_all_centers():
     insurances = db.session.query(Insurance).limit(10).all()
-    return [insurance.to_dict() for insurance in insurances]
+    return [insurance.to_dict(only=('id', 'name')) for insurance in insurances]
